4626b9d537007755f34aa92dce3558ac0e77cdb1
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 16:59:51 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// IF STATEMENT REVIEW
+// Day of the Week: Prompt the user to enter a day of the week (e.g., "Monday") and say whether it's a weekday or the weekend.
+
+
+
+
+
+// Age Group Classifier: Prompt the user to enter their age and categorize them into different age groups (child, teen, adult, senior)
+
+
+
+
+// WHILE LOOPS
+// Print 1 to 5
+
+
+
+
+// Print from a certain number to 1
+
+
+
+
+// Summing up to 5
+
+
+
+
+// Using prompt
+
+
+
+
+let mysterystudent = "Raeldo"
+let user2 = prompt("enter a student's name")
+
+let sum = 0
+let 
